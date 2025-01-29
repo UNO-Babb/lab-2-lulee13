2ae8f1eedd1fe1a4ace7908701c38777b8577ecd
lulee13 <lucaslee@unomaha.edu>
2025-01-29 16:04:06 +0000

    Time WIP

--- FutureTime.py
+++ FutureTime.py
@@ -16,13 +16,27 @@ def main():
 
   #TODO:
   #Ask user for hours
+  hoursAhead = input("Input hours ahead: ")
+
   #Ask user for minutes
+  minutesAhead = input("Input minutes ahead: ")
 
   #Calculate the time after the user-supplied time has passed.
+  
+  futureMinutes = (int(currentMinute) + int(minutesAhead)) % 60
+  futureHour = (int(currentHour) + int(hoursAhead) + (int(currentMinute + int(minutesAhead)) // 60)) % 24
 
   #Do not use any if statements in calculating the time.
 
   #Output the future time in standard format "HH:MM"
+  if futureHour > 9 and futureMinutes > 9:
+    print(futureHour + ":" + futureMinutes)
+  if futureHour < 10 and futureMinutes > 9:
+    print("0" + futureHour + ":" + futureMinutes)
+  if futureHour > 9 and futureMinutes < 10:
+    print(futureHour + ":0" + futureMinutes)
+  if futureHour < 10 and futureMinutes < 10:
+    print("0" + futureHour + ":0" + futureMinutes)
 
 
 if __name__ == '__main__':
